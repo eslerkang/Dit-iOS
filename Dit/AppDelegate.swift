0c24871f073ef154b92f454f7e7b82cae046a9ad
eslerkang <eslerkang@gmail.com>
2022-09-05 22:18:35 +0900

    initial setting again

--- Dit/AppDelegate.swift
+++ Dit/AppDelegate.swift
@@ -8,8 +8,6 @@
 import UIKit
 import CoreData
 
-import FirebaseCore
-
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,9 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
-        
         return true
     }
 
---
+++ Dit/TabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarController.swift
+//  Dit
+//
+//  Created by 강태준 on 2022/09/05.
+//
+
+import UIKit
+import SnapKit
+
+
+final class TabBarController: UITabBarController {
+    
+}
