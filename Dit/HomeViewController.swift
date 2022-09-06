35c9226148012204d26c9f5c88e6e6d74975b7cb
eslerkang <eslerkang@gmail.com>
2022-09-06 12:22:28 +0900

    feat: create adding, checking done todo function / feat: seperate done, todos

--- Dit/HomeViewController.swift
+++ Dit/HomeViewController.swift
@@ -13,6 +13,7 @@ import CoreData
 final class HomeViewController: UIViewController {
     var container: NSPersistentContainer!
     private var todos = [Todo]()
+    private var done = [Todo]()
     
     private lazy var searchController: UISearchController = {
         let searchController = UISearchController()
@@ -83,25 +84,51 @@ extension HomeViewController: UISearchBarDelegate {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todos.count
+        switch section {
+        case 0:
+            return todos.count
+        case 1:
+            return done.count
+        default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "\(todos.count) todos to commit"
+        switch section {
+        case 0:
+            return "\(todos.count) todos to commit"
+        case 1:
+            return "\(done.count) commits today"
+        default:
+            return ""
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         
-        var content = cell.defaultContentConfiguration()
-        content.text = todos[indexPath.row].text
-        content.image = UIImage(systemName: todos[indexPath.row].isDone ? "circle.fill" : "circle")
+        switch(indexPath.section) {
+        case 0:
+            var content = cell.defaultContentConfiguration()
+            content.text = todos[indexPath.row].text
+            content.image = UIImage(systemName: "circle")
+            
+            cell.contentConfiguration = content
+        case 1:
+            var content = cell.defaultContentConfiguration()
+            content.text = done[indexPath.row].text
+            content.image = UIImage(systemName: "circle.fill")
+            
+            cell.contentConfiguration = content
+        default:
+            return cell
+        }
         
-        cell.contentConfiguration = content
         cell.selectionStyle = .none
 
         return cell
@@ -122,25 +149,77 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var todo = todos[indexPath.row]
+        todo.isDone = true
+        
+        let readRequest = NSFetchRequest<NSManagedObject>(entityName: "Todos")
+        let isDonePredicate = NSPredicate(format: "isDone == NO")
+        let createdAtPredicate = NSPredicate(format: "createdAt == %@", todo.createdAt as CVarArg)
+        let textPredicate = NSPredicate(format: "text == %@", todo.text)
+        readRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [isDonePredicate, createdAtPredicate, textPredicate])
+        
+        do {
+            let data = try container.viewContext.fetch(readRequest)
+            let targetTodo = data[0]
+            
+            targetTodo.setValue(true, forKey: "isDone")
+            
+            try container.viewContext.save()
+        } catch {
+            print("ERROR: \(error.localizedDescription)")
+            return
+        }
+        
+        todos.remove(at: indexPath.row)
+        done.append(todo)
+        tableView.reloadData()
     }
 }
 
 
 private extension HomeViewController {
     func fetchTodos() {
-        let readRequest = NSFetchRequest<NSManagedObject>(entityName: "Todos")
-        let data = try! container.viewContext.fetch(readRequest)
+        let calnedar = Calendar(identifier: .gregorian)
+        let today = calnedar.startOfDay(for: Date())
+        guard let tomorrow = calnedar.date(byAdding: .day, value: 1, to: today)
+        else
+        {
+            return
+        }
         
-        self.todos = data.compactMap { todo in
+        let readTodosRequest = NSFetchRequest<NSManagedObject>(entityName: "Todos")
+        readTodosRequest.predicate = NSPredicate(format: "isDone == NO")
+        let todosData = try! container.viewContext.fetch(readTodosRequest)
+        
+        self.todos = todosData.compactMap { todo in
             guard let text = todo.value(forKey: "text") as? String,
                   let isDone = todo.value(forKey: "isDone") as? Bool,
                   let createdAt = todo.value(forKey: "createdAt") as? Date
             else {
                 return nil
             }
-            
             return Todo(text: text, isDone: isDone, createdAt: createdAt)
         }
+        
+        let readDoneRequest = NSFetchRequest<NSManagedObject>(entityName: "Todos")
+        let todayPredicate = NSPredicate(format: "createdAt >= %@", today as CVarArg)
+        let tomorrowPredicate = NSPredicate(format: "createdAt < %@", tomorrow as CVarArg)
+        let isDonePredicate = NSPredicate(format: "isDone == YES")
+        let compound = NSCompoundPredicate(andPredicateWithSubpredicates: [todayPredicate, tomorrowPredicate, isDonePredicate])
+        readDoneRequest.predicate = compound
+        let doneData = try! container.viewContext.fetch(readDoneRequest)
+        
+        self.done = doneData.compactMap { todo in
+            guard let text = todo.value(forKey: "text") as? String,
+                  let isDone = todo.value(forKey: "isDone") as? Bool,
+                  let createdAt = todo.value(forKey: "createdAt") as? Date
+            else {
+                return nil
+            }
+            return Todo(text: text, isDone: isDone, createdAt: createdAt)
+        }
+
+        tableView.reloadData()
     }
     
     func setupNavigation() {
