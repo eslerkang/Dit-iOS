4106911f3c7d10e65370a8325cc6f082cb20e207
eslerkang <eslerkang@gmail.com>
2022-09-08 01:56:00 +0900

    feat: create default UI / create monthCollectionView for this month's contributions

--- Dit/Scenes/ProfileViewController.swift
+++ Dit/Scenes/ProfileViewController.swift
@@ -14,11 +14,103 @@ final class ProfileViewController: UIViewController {
     var container: NSPersistentContainer!
     var context: NSManagedObjectContext!
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private lazy var monthCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionViewCell")
+        
+        return collectionView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .fill
+        
+        monthCollectionView.snp.makeConstraints {
+            $0.height.equalTo((view.frame.width - 40) / 7 * 5)
+        }
+        
+        [
+            monthCollectionView
+        ].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         container = appDelegate.persistentContainer
         context = container.viewContext
+        
+        setupNavigation()
+        setupLayout()
+    }
+}
+
+
+extension ProfileViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 35
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        cell.backgroundColor = .green
+        cell.layer.cornerRadius = 12
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 8
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+}
+
+
+private extension ProfileViewController {
+    func setupNavigation() {
+        navigationItem.title = "Contributions"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(20)
+        }
     }
 }
